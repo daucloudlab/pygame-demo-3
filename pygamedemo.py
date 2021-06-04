244050607c5db161071797654fe6b18ac9db2331
Daulet Baimbetov <daucloudlab@gmail.com>
2021-06-04 20:49:18 +0600

    программа каркасы орнатылды

---
+++ pygamedemo.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen_width = 500
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("PyGame Demo App")
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+    clock.tick(30)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            sys.exit()
+
